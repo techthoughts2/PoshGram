d296e73e304ab65d82949e9f29860fc217ad8941
Jake Morrison <tech@techthoughts.info>
2023-12-23 23:30:19 -0600

    bumped module versions

--- actions_bootstrap.ps1
+++ actions_bootstrap.ps1
@@ -15,17 +15,17 @@ $modulesToInstall = [System.Collections.ArrayList]::new()
 # https://github.com/pester/Pester
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'Pester'
-            ModuleVersion = '5.3.3'
+            ModuleVersion = '5.5.0'
         }))
 # https://github.com/nightroman/Invoke-Build
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'InvokeBuild'
-            ModuleVersion = '5.9.10'
+            ModuleVersion = '5.10.4'
         }))
 # https://github.com/PowerShell/PSScriptAnalyzer
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'PSScriptAnalyzer'
-            ModuleVersion = '1.20.0'
+            ModuleVersion = '1.21.0'
         }))
 # https://github.com/PowerShell/platyPS
 # older version used due to: https://github.com/PowerShell/platyPS/issues/457
